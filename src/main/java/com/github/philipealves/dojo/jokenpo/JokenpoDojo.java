026c3f2e80940b3b8dd42f147981d1306f2980e6
Philipe Alves de Oliveira e Silva <philipealvess@gmail.com>
2021-09-15 15:19:36 -0300

    Dojo first session

--- src/main/java/com/github/philipealves/dojo/jokenpo/JokenpoDojo.java
+++ src/main/java/com/github/philipealves/dojo/jokenpo/JokenpoDojo.java
@@ -1,5 +1,8 @@
 package com.github.philipealves.dojo.jokenpo;
 
+import com.github.philipealves.dojo.jokenpo.enums.Resultado;
+import com.github.philipealves.dojo.jokenpo.enums.TiposJogada;
+
 /**
  * Desafio Jokenpo: https://dojopuzzles.com/problems/jokenpo/
  * 
@@ -15,4 +18,22 @@ package com.github.philipealves.dojo.jokenpo;
  * empata com Papel e ganha de Pedra
  */
 public class JokenpoDojo {
+
+	public Resultado play(TiposJogada jogador1, TiposJogada jogador2) {
+        
+		if (jogador1.getPosicao() + jogador2.getPosicao() == jogador1.getPosicao() * 2)
+			return Resultado.EMPATA;
+
+		if (jogador1 == TiposJogada.TESOURA && jogador2 == TiposJogada.PEDRA) 
+			return Resultado.PERDE;
+
+		if (jogador1 == TiposJogada.PEDRA && jogador2 == TiposJogada.PAPEL)
+			return Resultado.PERDE;
+
+		if (jogador1 == TiposJogada.PAPEL && jogador2 == TiposJogada.TESOURA)
+			return Resultado.PERDE;
+				
+		return Resultado.GANHA;
+		
+	}
 }
--- src/test/java/com/github/philipealves/dojo/JokenpoDojoTest.java
+++ src/test/java/com/github/philipealves/dojo/JokenpoDojoTest.java
@@ -1,66 +1,69 @@
 package com.github.philipealves.dojo;
 
 import com.github.philipealves.dojo.jokenpo.JokenpoDojo;
+import com.github.philipealves.dojo.jokenpo.enums.Resultado;
+import com.github.philipealves.dojo.jokenpo.enums.TiposJogada;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Classe de teste para Dojo do desafio Jokenpo
- * Desafio: https://dojopuzzles.com/problems/jokenpo/
+ * Classe de teste para Dojo do desafio Jokenpo Desafio:
+ * https://dojopuzzles.com/problems/jokenpo/
  */
 public class JokenpoDojoTest {
 
     private JokenpoDojo jokenpo;
-
+   
     @Before
     public void setUp() {
         jokenpo = new JokenpoDojo();
     }
 
     @Test
-    public void pedra_empata_com_pedra() {
-        Assert.fail();
+    public void pedra_empata_com_pedra() {        
+        Assert.assertEquals(Resultado.EMPATA, jokenpo.play(TiposJogada.PEDRA, TiposJogada.PEDRA));
     }
 
     @Test
     public void tesoura_empata_com_tesoura() {
-        Assert.fail();
+        Assert.assertEquals(Resultado.EMPATA, jokenpo.play(TiposJogada.TESOURA, TiposJogada.TESOURA));
     }
 
     @Test
     public void papel_empata_com_papel() {
-        Assert.fail();
+        Assert.assertEquals(Resultado.EMPATA, jokenpo.play(TiposJogada.PAPEL, TiposJogada.PAPEL));
     }
 
     @Test
     public void pedra_ganha_de_tesoura() {
-        Assert.fail();
+        Assert.assertEquals(Resultado.GANHA, jokenpo.play(TiposJogada.PEDRA, TiposJogada.TESOURA));
     }
 
     @Test
     public void tesoura_ganha_de_papel() {
-        Assert.fail();
+        Assert.assertEquals(Resultado.GANHA, jokenpo.play(TiposJogada.TESOURA, TiposJogada.PAPEL));
     }
 
     @Test
     public void papel_ganha_de_pedra() {
-        Assert.fail();
+        Assert.assertEquals(Resultado.GANHA, jokenpo.play(TiposJogada.PAPEL, TiposJogada.PEDRA));
     }
 
     @Test
     public void tesoura_perde_de_pedra() {
-        Assert.fail();
+        Assert.assertEquals(Resultado.PERDE, jokenpo.play(TiposJogada.TESOURA, TiposJogada.PEDRA));
     }
 
     @Test
     public void pedra_perde_de_papel() {
-        Assert.fail();
+        Assert.assertEquals(Resultado.PERDE, jokenpo.play(TiposJogada.PEDRA, TiposJogada.PAPEL));
     }
 
     @Test
     public void papel_perde_de_tesoura() {
-        Assert.fail();
+        Assert.assertEquals(Resultado.PERDE, jokenpo.play(TiposJogada.PAPEL, TiposJogada.TESOURA));
     }
 
 }
